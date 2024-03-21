9ff6fb38de7142523ad1a9313ed3d512109da828
anandakbari <anandakbari9@gmail.com>
2024-03-21 22:03:54 +0530

    jpa first commit

--- src/main/java/com/example/database/domain/Author.java
+++ src/main/java/com/example/database/domain/Author.java
@@ -1,17 +1,20 @@
 package com.example.database.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name="authors")
 public class Author {
-
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,generator="author_id_seq")
     private Long id;
+
     private String name;
+
     private Integer age;
 }
--- src/main/java/com/example/database/domain/Book.java
+++ src/main/java/com/example/database/domain/Book.java
@@ -1,5 +1,6 @@
 package com.example.database.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
+@Entity
+@Table(name="books")
 public class Book {
+
+    @Id
     private String isbn;
+
     private String title;
-    private Long authorId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Author author;
 }
